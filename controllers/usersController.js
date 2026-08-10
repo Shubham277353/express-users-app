35723b6a42e0b350023c472601d117a5d407e581
Shubham <golubhandari405@gmail.com>
2026-08-10 16:03:59 +0000

    feat: add more details for users

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -30,25 +30,37 @@ const validateUser = [
     .withMessage(`Last name ${alphaErr}`)
     .isLength({ min: 1, max: 10 })
     .withMessage(`Last name ${lengthErr}`),
+  body("email")
+    .trim()
+    .isEmail()
+    .withMessage("please enter a valid email address."),
+  body("age")
+    .optional({ values: "falsy" })
+    .isInt({ min: 18, max: 120 })
+    .withMessage("Age must be between 18 and 120."),
+  body("bio")
+    .optional({ values: "falsy" })
+    .trim()
+    .isLength({ max: 200 })
+    .withMessage("Bio must be 200 characters or less."),
 ];
 
 exports.usersCreatePost = [
   validateUser,
   (req, res) => {
     const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).render("createUser",{
-            title: "Create User",
-            errors: errors.array(),
-        });
+    if (!errors.isEmpty()) {
+      return res.status(400).render("createUser", {
+        title: "Create User",
+        errors: errors.array(),
+      });
     }
 
-    const { firstName, lastName } = matchedData(req);
-    usersStorage.addUser({ firstName, lastName });
+    const { firstName, lastName, email, age, bio } = matchedData(req);
+    usersStorage.addUser({ firstName, lastName, email, age, bio });
     res.redirect("/");
-  },    
+  },
 ];
-
 
 exports.usersUpdateGet = (req, res) => {
   const user = usersStorage.getUser(req.params.id);
@@ -70,15 +82,13 @@ exports.usersUpdatePost = [
         errors: errors.array(),
       });
     }
-    const { firstName, lastName } = matchedData(req);
-    usersStorage.updateUser(req.params.id, { firstName, lastName });
+    const { firstName, lastName, email, age, bio } = matchedData(req);
+    usersStorage.updateUser(req.params.id, { firstName, lastName, email, age, bio });
     res.redirect("/");
-  }
+  },
 ];
 
 exports.usersDeletePost = (req, res) => {
   usersStorage.deleteUser(req.params.id);
   res.redirect("/");
 };
-
-
